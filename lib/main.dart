88e8afeb2fa64f37f45fe220f8ecc6fef19a7300
Sasikumar Arumugam <sasikumar.arumugam@syncfusion.com>
2021-04-05 19:31:25 +0530

    FLUT-4313-Sample updated with null safety.

--- lib/main.dart
+++ lib/main.dart
@@ -23,36 +23,17 @@ class HeaderCustomization extends StatefulWidget {
 }
 
 class _HeaderCustomizationState extends State<HeaderCustomization> {
-  DateRangePickerController _controller;
-  int _startDate;
-  String headerString;
-  DateTime midDate;
-  double width, cellWidth, height, cellHeight;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    _controller = DateRangePickerController();
-    _startDate = 0;
-    width = 0.0;
-    cellWidth = 0.0;
-    height = 0.0;
-    cellHeight = 0.0;
-    midDate = DateTime.now();
-    headerString = '';
-    super.initState();
-  }
+  final DateRangePickerController _controller = DateRangePickerController();
+  String headerString = '';
 
   @override
   Widget build(BuildContext context) {
-    width = MediaQuery.of(context).size.width;
-    cellWidth = width / 9;
-    height = MediaQuery.of(context).size.height;
-    cellHeight = height / 17;
+    final double width = MediaQuery.of(context).size.width;
+    final double cellWidth = width / 9;
 
     return Scaffold(
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Row(
           children: <Widget>[
@@ -71,7 +52,7 @@ class _HeaderCustomizationState extends State<HeaderCustomization> {
                   highlightColor: Colors.lightGreen,
                   onPressed: () {
                     setState(() {
-                      _controller.backward();
+                      _controller.backward!();
                     });
                   },
                 )),
@@ -94,7 +75,7 @@ class _HeaderCustomizationState extends State<HeaderCustomization> {
                   highlightColor: Colors.lightGreen,
                   onPressed: () {
                     setState(() {
-                      _controller.forward();
+                      _controller.forward!();
                     });
                   },
                 )),
@@ -118,22 +99,23 @@ class _HeaderCustomizationState extends State<HeaderCustomization> {
               monthCellStyle: DateRangePickerMonthCellStyle(
                   cellDecoration: BoxDecoration(color: Color(0xFF6fb98f)),
                   leadingDatesDecoration:
-                  BoxDecoration(color: Color(0xFF6fb98f)),
+                      BoxDecoration(color: Color(0xFF6fb98f)),
                   trailingDatesDecoration:
-                  BoxDecoration(color: Color(0xFF6fb98f)))),
+                      BoxDecoration(color: Color(0xFF6fb98f)))),
         )
       ],
     ));
   }
 
   void viewChanged(DateRangePickerViewChangedArgs args) {
-    _startDate = (args.visibleDateRange.startDate
-        .difference(args.visibleDateRange.endDate)
-        .inDays);
-    var middleDate = (_startDate ~/ 2).toInt();
-    midDate = args.visibleDateRange.startDate.add(Duration(days: middleDate));
+    final DateTime visibleStartDate = args.visibleDateRange.startDate!;
+    final DateTime visibleEndDate = args.visibleDateRange.endDate!;
+    final int totalVisibleDays =
+        (visibleStartDate.difference(visibleEndDate).inDays);
+    final DateTime midDate =
+        visibleStartDate.add(Duration(days: totalVisibleDays ~/ 2));
     headerString = DateFormat('MMMM yyyy').format(midDate).toString();
-    SchedulerBinding.instance.addPostFrameCallback((duration) {
+    SchedulerBinding.instance!.addPostFrameCallback((duration) {
       setState(() {});
     });
   }
